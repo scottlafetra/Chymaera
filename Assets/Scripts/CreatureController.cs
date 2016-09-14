a5ea0551c3273c839d836adaa131c8fca07fb759
Scott La Fetra <scottlafetra@gmail.com>
2016-09-14 14:15:10 -0700

    Inital Commit

---
+++ Assets/Scripts/CreatureController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureController : MonoBehaviour
+{
+    public enum StatusEffect
+    {
+        Burn, Confusion, Fear, Freeze, Irradiate, Poisioned
+    }
+
+    //TODO replace test values
+    public double AttackSpeed
+    {
+        get { return 7; }
+    }
+
+    //TODO replace test values
+    public double Health
+    {
+        get { return 77; }
+    }
+
+    //TODO replace test values
+    public double Dodge
+    {
+        get { return 7; }
+    }
+
+    //TODO replace test values
+    public double PhysicalDefense
+    {
+        get { return 7; }
+    }
+
+    //TODO replace test values
+    public double MagicDamage
+    {
+        get { return 7; }
+    }
+
+    //TODO replace test values
+    public double MagicDefense
+    {
+        get { return 7; }
+    }
+
+    //TODO replace test values
+    public double CriticalPercent
+    {
+        get { return 7; }
+    }
+}
